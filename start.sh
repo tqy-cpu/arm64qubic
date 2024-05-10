52ab27a9a81e1ca45d8d67206dad140fee28ca4d
tqy-cpu <116723359+tqy-cpu@users.noreply.github.com>
2024-05-11 00:59:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ yes | apt install wget
 
 rm rqiner-aarch64-mobile
 
-wget "https://gitee.com/jingzhi587/rqiner-builds/releases/tag/rqiner/$latestversion/rqiner-aarch64-mobile"
+wget "https://gitee.com/jingzhi587/rqiner-builds/releases/download/rqiner/rqiner-aarch64-mobile"
 
 chmod +x rqiner-aarch64-mobile
 
